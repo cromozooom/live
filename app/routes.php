384df79d4a47de7044662295d643afe936c50a7d
unknown <nathi.taengnim@PC000138.gimo.local>
2016-04-26 15:39:52 +0100

    Change routes code

--- app/routes.php
+++ app/routes.php
@@ -1,6 +1,4 @@
 <?php
 
 // Define named route
-$app->get('/', function ($request, $response, $args) {
-  return $this->view->render($response, 'home.tpl');
-})->setName('home');
+$app->get('/', 'HomeController:index')->setName('home');
