098fc5521f4ed08a190e6e3dc0c013a7fcda5ad2
Nathi Taengnim <nathi.t@outlook.com>
2016-04-26 23:11:34 +0100

    create odds functioin

--- app/Models/Odds.php
+++ app/Models/Odds.php
@@ -4,18 +4,51 @@ namespace App\Models;
 
 class Odds
 {
-    protected $branchid;
+    protected $BranchId;
     protected $EventType;
     protected $OddsStyle;
 
-    public function __construct($branchid , $EventType, $OddsStyle)
+    public function __construct($branchid , $EventType, $OddsStyle )
     {
-      $this->branchid = $branchid;
+      $this->BranchId = $BranchId;
       $this->EventType = $EventType;
       $this->OddsStyle = $OddsStyle;
     }
+    public function getBranchId(){
+      return $this->BranchId;
+    }
 
+    public function geteventType(){
+      return $this->EventType;
+    }
+
+    public function getOddsStyle(){
+      return $this->OddsStyle;
+    }
     public function getXmlUrl(){
-      	$url1="http://netbetxml.sbtech.com/lines.aspx?branchid=6&EventType=0&OddsStyle=DECIMAL";
+      	$url="http://netbetxml.sbtech.com/lines.aspx?branchid=".$this->BranchId."&EventType=".$this->EventType."&OddsStyle=".$this->OddsStyle;
+
+        return $url;
+    }
+
+    public function getXmlObject(){
+
+      $url = $this->getXmlUrl();
+      $ch = curl_init();
+
+  		curl_setopt_array($ch, array(
+  		    CURLOPT_URL => $url
+  		  , CURLOPT_HEADER => 0
+  		  , CURLOPT_RETURNTRANSFER => 1
+  		  , CURLOPT_ENCODING => 'gzip'
+  		));
+  		$xml = curl_exec($ch);
+  		//print_r($xml);
+  		//die();
+  		curl_close($ch);
+
+  		$xmlFeeds = new SimpleXMLElement($xml);
+
+      return $xmlFeeds;
     }
 }
